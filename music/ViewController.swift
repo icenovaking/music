0f009320399a804419f3e4b7b01dea5a79e7551c
icenova <iceorbbroy@gmail.com>
2019-07-14 16:51:22 +0800

    Initial Commit

---
+++ music/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  music
+//
+//  Created by roy on 2019/7/14.
+//  Copyright © 2019 roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
